9d52d8e0550dd95cc2e1b1b02f4ead251886c941
AM-AM <38333315+AM-AM@users.noreply.github.com>
2020-02-29 19:38:28 -0600

    initial commits

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ setInterval(function() {
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
 
 
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -44,7 +45,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
-    
+    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
